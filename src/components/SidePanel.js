50ce3e4369faf17b01fd008856cdc273d5083b5d
John Doe <johndoe@email.com>
2023-08-10 17:48:57 +0530

    sidepanel updated

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -5,7 +5,28 @@ import './SidePanel.css'; // Import your CSS file for styling
 function SidePanel() {
   return (
     <div className="side-panel">
-      {/* Sidebar content */}
+      <div className="profile-section">
+        <div className="profile-photo">
+          {/* Replace with your profile photo */}
+        </div>
+        <div className="role">Warehouse Administrator</div>
+      </div>
+      <div className="menu-section">
+      <div className="sub-heading active">Dashboard</div>
+        <div className="section">
+          <h2 className="section-heading">Products</h2>
+          <div className="sub-heading">Create Products</div>
+          <div className="sub-heading">Category</div>
+          {/* ... (more subheadings) */}
+        </div>
+        <div className="section">
+          <h2 className="section-heading">Inventory</h2>
+          <div className="sub-heading">Order Requests</div>
+          <div className="sub-heading">Charts</div>
+          {/* ... (more subheadings) */}
+        </div>
+        {/* ... (more sections) */}
+      </div>
     </div>
   );
 }
